91ec6e7f0b23c853a94c5baa79f46b8c702f034c
JakubWronowski <132200637+JakubWronowski@users.noreply.github.com>
2023-05-13 09:25:18 +0200

    Add files via upload

---
+++ S2S_vpn_aws_azure/terraform_azure_vpn_gateway.tf
@@ -0,0 +1,32 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "resource_group" {
+  name     = "azure_resource_group"
+  location = "West US"
+}
+
+resource "azurerm_virtual_network" "virtual_network" {
+  name                = "azure_vnet"
+  address_space       = ["10.1.0.0/16"]
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+}
+
+resource "azurerm_virtual_network_gateway" "vpn_gateway" {
+  name                = "azure_vpn_gateway"
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+
+  type     = "Vpn"
+  vpn_type = "RouteBased"
+
+  ip_configuration {
+    name                          = "vnetGatewayConfig"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+    private_ip_address_allocation = "Dynamic"
+    subnet_id                     = azurerm_subnet.gateway_subnet.id
+  }
+}
+
