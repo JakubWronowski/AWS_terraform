6e7a2ae166d19db6657eebdec7db682084f2a858
JakubWronowski <132200637+JakubWronowski@users.noreply.github.com>
2023-05-13 09:24:29 +0200

    Update terraform_aws_vpn_gateway.tf

--- S2S_vpn_aws_azure/terraform_aws_vpn_gateway.tf
+++ S2S_vpn_aws_azure/terraform_aws_vpn_gateway.tf
@@ -1,1 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~>4.65.0"
+    }
+  }      
+}
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_vpc" "vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "aws_vpc"
+  }
+}
+
+resource "aws_vpn_gateway" "vpn_gateway" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "aws_vpn_gateway"
+  }
+}
+
+
 
