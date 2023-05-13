32ec1c7b0cb5c6d7a0bfe52547b7e07dd5060dc0
JakubWronowski <132200637+JakubWronowski@users.noreply.github.com>
2023-05-13 09:34:37 +0200

    Create outputs.tf

---
+++ S2S_vpn_aws_azure/outputs.tf
@@ -0,0 +1,9 @@
+outputs "aws_vpn_gateway_details" {
+  value = aws_vpn_gateway.vpn_gateway
+}
+
+
+outputs "azurerm_virtualnet_gateway_details" {
+  value = azurerm_virtual_network_gateway.vpn_gateway
+}
+
